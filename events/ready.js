969b3ebe39ca4e1fa86a0905ac8c078f494f36b0
Panquesito7 <halfpacho@gmail.com>
2021-09-16 16:43:42 +0000

    Update bot status to `Playing with code!`

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,7 @@
 module.exports = async (client) => {
     console.log(`==== Bot ready :: ${client.user.username} =====`)
-    const setStatus = () => client.user.setActivity(`Dev OP!`)
+    const setStatus = () => client.user.setActivity(`with code!`, {
+        type: "PLAYING",
+    })
     setStatus()
 }
