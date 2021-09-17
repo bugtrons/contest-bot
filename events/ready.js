a533057bfc5c51b3e8fc88fd0b2e8e43895b44a1
Darshan Patel <heyforscht@gmail.com>
2021-09-18 03:14:28 +0530

    [MOD] Removed unnecessary code, make status command dynamic and enable global commands

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,6 @@
 module.exports = async (client) => {
     console.log(`==== Bot ready :: ${client.user.username} =====`)
-    const setStatus = () => client.user.setActivity(`Dev OP!`)
+    const status = process.env.BOT_STATUS || 'with code!'
+    const setStatus = () => client.user.setActivity(status, { type: 'PLAYING' })
     setStatus()
 }
--- util/errorPrint.js
+++ util/errorPrint.js
@@ -1,7 +1,7 @@
 module.exports = (error, extra = {}) => {
     let err = `${'=== Begin Error ===\n---\n'
         + 'Error: '}${error.message}\n`
-    const extraArray = Object.keys(extra).map(e => `${e} : ${extra[e]}`).join('\n')
+    const extraArray = Object.keys(extra).map((e) => `${e} : ${extra[e]}`).join('\n')
     err += extraArray
     err += `\nStack: ${error.stack}\n---\n=== End Error ===`
 
